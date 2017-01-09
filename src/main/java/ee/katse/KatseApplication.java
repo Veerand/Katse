6d633fdbd536b4a4506adc61b85d6b42ba460d7e
Veerand <s.neljandik@gmail.com>
2017-01-09 19:21:14 +0200

    Add files via upload

---
+++ src/main/java/ee/katse/KatseApplication.java
@@ -0,0 +1,12 @@
+package ee.katse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KatseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KatseApplication.class, args);
+	}
+}
